ccf79dba2430e4ad3eb6a8be9dab3cfe7b9ed375
Makar <makardovgopolji@gmail.com>
2025-11-28 14:31:25 +0100

    feat: init redis, main.ts and gql

--- src/core/prisma/prisma.service.ts
+++ src/core/prisma/prisma.service.ts
@@ -1,8 +1,18 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+  }
+
   public async onModuleInit() {
     await this.$connect();
   }
